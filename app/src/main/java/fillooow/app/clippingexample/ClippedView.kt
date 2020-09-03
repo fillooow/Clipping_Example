3064328504d50059528b8ffef7d0e82bd98abcdd
fillooow <fil201121@yandex.ru>
2020-09-03 15:45:56 +0500

    нарисовал прямоугольник с нужными мне фигурами, сделал заготовки на будущее

--- app/src/main/java/fillooow/app/clippingexample/ClippedView.kt
+++ app/src/main/java/fillooow/app/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package fillooow.app.clippingexample
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -14,12 +16,106 @@ class ClippedView @JvmOverloads constructor(
 
 ) : View(context, attrs, defStyleAttr) {
 
-    private val path = Path()
-
     private val paint = Paint().apply {
 
         isAntiAlias = true
         strokeWidth = resources.getDimension(R.dimen.strokeWidth)
         textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
+
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawBackAndUnclippedRectangle(canvas)
+        drawDifferenceClippingExample(canvas)
+        drawCircularClippingExample(canvas)
+        drawIntersectionClippingExample(canvas)
+        drawCombinedClippingExample(canvas)
+        drawRoundedRectangleClippingExample(canvas)
+        drawOutsideClippingExample(canvas)
+        drawSkewedTextExample(canvas)
+        drawTranslatedTextExample(canvas)
+        // drawQuickRejectExample(canvas)
+    }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+
+        // Вырезаем маленький прямоугольник и заливаем его белым
+        canvas.clipRect(clipRectLeft, clipRectTop, clipRectRight, clipRectBottom)
+        canvas.drawColor(Color.WHITE)
+
+        // Рисуем в этом прямоугольнике красную диагональную линию
+        paint.color = Color.RED
+        canvas.drawLine(clipRectLeft, clipRectTop, clipRectRight, clipRectBottom, paint)
+
+        // Рисуем зеленую окружность
+        paint.color = Color.GREEN
+        canvas.drawCircle(circleRadius, clipRectBottom - circleRadius, circleRadius, paint)
+
+        // Рисуем синий текст в правой части
+        paint.color = Color.BLUE
+        paint.textSize = textSize
+        // Если было бы LEFT, текст бы выровнялся по левой стороне слова и был бы за границей прямоугольника
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(context.getString(R.string.clipping), clipRectRight, textOffset, paint)
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+
+        canvas.drawColor(Color.GRAY)
+        canvas.save()
+        canvas.translate(columnOne,rowOne)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+
+    private fun drawDifferenceClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCircularClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawIntersectionClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCombinedClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawOutsideClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawTranslatedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawSkewedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawQuickRejectExample(canvas: Canvas) {
     }
 }
