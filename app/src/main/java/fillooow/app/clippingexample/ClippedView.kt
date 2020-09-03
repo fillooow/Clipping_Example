898643ec79be00e78edadcf9f149f15b49996888
fillooow <fil201121@yandex.ru>
2020-09-03 15:14:02 +0500

    Initial commit

---
+++ app/src/main/java/fillooow/app/clippingexample/ClippedView.kt
@@ -0,0 +1,25 @@
+package fillooow.app.clippingexample
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+
+) : View(context, attrs, defStyleAttr) {
+
+    private val path = Path()
+
+    private val paint = Paint().apply {
+
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+}
